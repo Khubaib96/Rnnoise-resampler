1d44bec6ce395975f81a8539c099d86a8eccf750
Khubaib Ahmad <khubaib10@gmail.com>
2023-03-30 01:47:12 +0500

    changes made in cmakelists

--- cmake-build-debug/CMakeFiles/finalCode.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/finalCode.dir/DependInfo.cmake
@@ -1,29 +1,8 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/celt_lpc.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/celt_lpc.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/denoise.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/denoise.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/kiss_fft.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/kiss_fft.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/models.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/models.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/models/bd.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/models/bd.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/models/cb.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/models/cb.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/models/lq.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/models/lq.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/models/mp.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/models/mp.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/models/sh.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/models/sh.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/pitch.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/pitch.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/rnn.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/rnn.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/rnn_data.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/rnn_data.c.o"
-  "/mnt/s/UpworkProjects/DavidAGC/finalCode/rnnoise/src/rnn_reader.c" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/rnnoise/src/rnn_reader.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/mnt/s/UpworkProjects/DavidAGC/finalCode/main.cpp" "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/CMakeFiles/finalCode.dir/main.cpp.o"
   )
@@ -35,6 +14,8 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/rnnoise/CMakeFiles/rnnoise.dir/DependInfo.cmake"
+  "/mnt/s/UpworkProjects/DavidAGC/finalCode/cmake-build-debug/resampling/CMakeFiles/resampling.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
